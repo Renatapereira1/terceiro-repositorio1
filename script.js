9f33b468cb3fe5bdaa2cb0db06e34422b2d1e889
Renatapereira1 <pereira.costa.renata@escola.pr.gov.br>
2025-04-07 22:49:19 -0300

    aula 07/04

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){
+
+         })
+         const botaoAcessibilidade = document.getElementById
+         ('botao-acessibilidade');
+         const opcoesDeAcessibilidade = document.getElementById
+         ('opcoes-acessibilidade');
+    })
